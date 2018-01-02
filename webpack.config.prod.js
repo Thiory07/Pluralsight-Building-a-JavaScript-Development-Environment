38fb879ec07b513a91cbc6d9f6e77d12d97dba25
Ricardo Gimenes Ferreira <gimenesferreira@gmail.com>
2018-01-02 14:11:40 -0200

    added chunking to js codes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,25 +1,48 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[chunkhash].css'),
+    //Generate an external css file with a hash in the filename
+    new WebpackMd5Hash(),
+
+    // Use CommonsChunkPlugin to create a separate bundle of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     // Create HTML file that includes a reference to bundled js
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      minify:{
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes:true,
+        keepClosingSlash:true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
       inject:true
     }),
     // MinifyJs
@@ -30,7 +53,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
     ]
   }
 }
